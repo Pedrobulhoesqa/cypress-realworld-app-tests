b5203547fa45d0f6792705b8c6dba4a5b67f591e
Pedrobulhoesqa <pedrobulhoesdias@gmail.com>
2025-03-12 22:49:42 -0300

    Added Register and login user automation

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,5 +1,21 @@
   describe('Login com sucesso', () => {
    it('Deve fazer login com um usuário válido', () => {
-      cy.visit('/')
+      cy.visit('/signin')
+      cy.get("[data-test='signin-username']").type("test1user")
+      cy.get("[data-test='signin-password']").type("test123")
+      cy.get("[data-test='signin-submit']").should("be.enabled").click()
+      cy.location('pathname').should('equal', '/')
+      cy.get("[data-test='user-onboarding-dialog-title']").should("be.visible").contains("Get Started with Real World App")
+      cy.get("[data-test='user-onboarding-next']").click()
+      cy.get("[data-test='user-onboarding-dialog-title']").should("be.visible").contains("Create Bank Account")
+      cy.get("[data-test='bankaccount-bankName-input']").type("test-1")
+      cy.get("[data-test='bankaccount-routingNumber-input']").type("test-1234")
+      cy.get("[data-test='bankaccount-accountNumber-input']").type("test-1234")
+      cy.get("[data-test='bankaccount-submit']").should("be.enabled").click()
+      cy.get("[data-test='user-onboarding-dialog-title']").contains("Finished")
+      cy.get("[data-test='user-onboarding-next']").click()
+      cy.get('[data-test="sidenav-username"]').contains("test1user")
+      cy.get('[data-test="nav-transaction-tabs"]').should('exist')
+
    });
  });
--- cypress/tests/e2e/register.spec.js
+++ cypress/tests/e2e/register.spec.js
@@ -1,6 +1,31 @@
+import { be } from "date-fns/locale"
+
 describe('Registro de usuário com sucesso', () => {
     it('Criar o usuário corretamente', () => {
        cy.visit('/')
        cy.get("[data-test='signup']").click()
+       cy.location('pathname').should('equal', '/signup')
+       cy.get("[data-test='signup-first-name']").type("test")
+       cy.get("[data-test='signup-last-name']").type("1")
+       cy.get("[data-test='signup-username']").type("test1user")
+       cy.get("[data-test='signup-password']").type("test123")
+       cy.get("[data-test='signup-confirmPassword']").type("test123")
+       cy.get("[data-test='signup-submit']").should("be.enabled").click()
+       cy.location('pathname').should('equal', '/signin')
   })
 })
+
+describe('Tentar registrar um novo usuário com informações incompletas', () => {
+  it.only('Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias', () => {
+    cy.visit('/')
+       cy.get("[data-test='signup']").click()
+       cy.location('pathname').should('equal', '/signup')
+       cy.get("[data-test='signup-first-name']").type("1")
+       cy.get("[data-test='signup-last-name']").type("1")
+       cy.get("[data-test='signup-username']").type("1")
+       cy.get("[data-test='signup-password']").type("1")
+       cy.get("#password-helper-text").should("be.visible")
+       cy.get("[data-test='signup-confirmPassword']").type("1")
+       cy.get("[data-test='signup-submit']").should("be.disabled")
+  });
+});
